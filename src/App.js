0cb583869d1fb13f0f06429a224a192ca7c7e11e
Rana-Elgohary <r.elgohary2000@gmail.com>
2023-07-05 22:31:50 +0300

    A React.js dashboard using tailwind and syncfusion UI Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
+import { BrowserRouter } from "react-router-dom"
+import {TooltipComponent} from "@syncfusion/ej2-react-popups"
+import {FiSettings} from "react-icons/fi"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div className="flex right-4 bottom-4 z-50">
+          <TooltipComponent content="hehe" position="Top">
+            <button type="button" className="text-3xl p-3 hover:drop-shadow-xl">
+              <FiSettings/>
+            </button>
+          </TooltipComponent>
+        </div>
+      </div>
+      </BrowserRouter>
     </div>
   );
 }
